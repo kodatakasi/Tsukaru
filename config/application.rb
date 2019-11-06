e1928583a82bf836ad60cd0545b10affdd535cfc
koda takasi <kodatakasi@gmail.com>
2019-11-06 14:32:38 +0900

    gem bundle

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module Tsukaru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
