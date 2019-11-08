14d9d637fe1061a17692cf310472f9b01b418df5
koda takasi <kodatakasi@gmail.com>
2019-11-08 10:12:35 +0900

    devise controller実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def authenticate_user!
-    session[:user_return_to] = env['PATH_INFO']
-    redirect_to user_omniauth_authorize_path(:facebook) unless user_signed_in?
-  end
-  
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
