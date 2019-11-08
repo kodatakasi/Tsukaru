61e14a91808417f661dd6a61348baaa3b73112b5
koda takasi <kodatakasi@gmail.com>
2019-11-08 10:23:47 +0900

    devise view form_forに戻して一旦復旧

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles
-  get "home", to: "article#index", as: "user_root"
+  get "home", to: "article#index"
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
