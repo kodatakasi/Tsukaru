22c77cb571f261f3ab8b7da496609f6077e58bed
koda takasi <kodatakasi@gmail.com>
2019-11-20 18:15:31 +0900

    line auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook line]
 
   # ユーザーをフォローする
   def follow!(other_user)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,6 +262,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET_KEY"], token_params: { parse: :json }, scope: 'email,name,image'
 
+  config.omniauth :line, ENV["LINE_ID"], ENV["LINE_SECRET_KEY"], token_params: { parse: :json }, scope: 'email,name,image'
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
