1aeec78f54e21f81bbdfd231347f405656378f52
koda takasi <kodatakasi@gmail.com>
2019-11-20 12:46:59 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,16 +35,19 @@ class User < ApplicationRecord
     active_relationships.find_by(followed_id: other_user.id)
   end
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+ 
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
     end
+ 
+    user
   end
 
   private
